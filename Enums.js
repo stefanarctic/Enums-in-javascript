97430b53523a794d7929499a7a77ae92eb6ccce8
stefanarctic <87850525+stefanarctic@users.noreply.github.com>
2022-02-15 22:15:17 +0200

    Update Enums.js

--- Enums.js
+++ Enums.js
@@ -10,7 +10,7 @@ class EnumInstance {
         this.property = property;
     }
     compare(property) {
-        return this.property == property;
+        return this.property === property;
     }
 }
 
